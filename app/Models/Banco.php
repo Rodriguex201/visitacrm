63c21621be14ee3b1c09eeb4c26919439224a413
Rodriguex201 <emanuelgr150@gmail.com>
2026-02-20 15:25:36 -0500

    Add bancos catalog support to usuarios forms and table

---
+++ app/Models/Banco.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Banco extends Model
+{
+    protected $table = 'bancos';
+
+    protected $fillable = [
+        'nombre',
+        'activo',
+    ];
+}
