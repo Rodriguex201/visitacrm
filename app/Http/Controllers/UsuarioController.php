1e90880a4c7eacb7818cb703333eed66e7f86cc1
Rodriguex201 <emanuelgr150@gmail.com>
2026-03-04 09:57:54 -0500

    Corregir apertura de modal en engranaje de referidos

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -57,10 +57,24 @@ class UsuarioController extends Controller
     public function referidos(User $user): View
     {
         $empresasReferidas = $user->empresasReferidas()
-            ->with(['sector'])
+            ->with([
+                'sector',
+                'opciones' => function ($query): void {
+                    $query->select('catalogo_opciones.id', 'catalogo_opciones.categoria', 'catalogo_opciones.nombre', 'catalogo_opciones.valor')
+                        ->whereNotIn('catalogo_opciones.categoria', ['Cotizaciones', 'Como Llego'])
+                        ->orderBy('catalogo_opciones.categoria')
+                        ->orderBy('catalogo_opciones.nombre');
+                },
+            ])
             ->latest('referida_at')
             ->latest('id')
             ->paginate(10);
+
+        $empresasReferidas->getCollection()->transform(function ($empresa) {
+            $empresa->valor_total_referido = (float) $empresa->opciones->sum('valor');
+
+            return $empresa;
+        });
 
         return view('usuarios.referidos', compact('user', 'empresasReferidas'));
     }
