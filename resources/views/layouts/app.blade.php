35d2ffc889097f789c63f41f63b550d11c4b1cb7
Emanuel García Rodríguez <emanuelgr150@gmail.com>
2026-03-17 09:35:54 -0500

    cambios de logo nombre

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'VisitaCRM') }}</title>
+<title>{{ config('app.name', 'VisitaCRM') }}</title>
+
+<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=99">
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v=99">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png') }}?v=99">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.png') }}?v=99">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet" />
